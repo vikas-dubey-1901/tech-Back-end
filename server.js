4a06880f08453f7092bd02a882d17530d3ff04c7
vikas-dubey-1901 <dubey.vikas5003@gmail.com>
2020-06-29 21:13:19 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,31 @@
+
+const mongoose = require('mongoose');
+
+const dotenv = require('dotenv');
+dotenv.config({ path: './config.env' });
+
+const app = require('./app');
+
+// DATABASE CONNECTION
+
+const DB = process.env.DATABASE.replace(
+  '<password>',
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true
+  })
+  .then(() => console.log('Database connection successful!'));
+
+app.get('/', (req, res) => res.send('hello world!'));
+
+//CREATING SERVER
+const port = process.env.PORT;
+const server = app.listen(port, () => {
+  console.log(`App running on PORT ${port}..`);
+});
